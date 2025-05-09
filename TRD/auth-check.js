5d08b4498a167c00118f7fd0e64a3574bd35305a
Sebastian Suarez <sebas1019sebo@gmail.com>
2025-05-09 17:15:58 -0400

    este es nueviño

--- TRD/auth-check.js
+++ TRD/auth-check.js
@@ -1,6 +1,7 @@
+// auth-check.js: verifica que haya un usuario autenticado en Firebase
 firebase.auth().onAuthStateChanged((user) => {
-    if (!user) {
-      window.location.href = "../../index.html";
-    }
-  });
-  
+  if (!user) {
+    // No hay usuario autenticado: redirige a la página de inicio
+    window.location.replace('../../index.html');
+  }
+});
